ddc16249d75c061293e6f599ca6e64bb1f4c0c78
Nicolas Lamblin <nicolas.lamblin2@gmail.com>
2018-01-09 13:54:23 +0100

    Translate

--- gobatch.py
+++ gobatch.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*
+
 import os
 import sys
 import signal
@@ -7,11 +8,12 @@ import time
 import datetime
 import calendar
 
-                                                #############
-                                                # FUNCTIONS #
-                                                #############
 
-#Fonction permettant de lire le fichier fbatch
+#############
+# FUNCTIONS #
+#############
+
+# Function to read the fbatch file
 def readFile():
     with open("FBatch", "r") as f:
         array = []
@@ -20,14 +22,16 @@ def readFile():
             array.append(line.split('\t'))
     return array
 
-#Fonction qui convertit en seconde entre le jour courant et le prochain declenchement
-#en fonction des paramètres inscrit dans fbatch
+
+# Function that converts in seconds between the current day and the next trigger
+# according to the parameters entered in fbatch
 def convertInSecond(minute, heure, jour, mois, repet):
     now = datetime.datetime.now()
+
     if repet == "daily":
         tomorrow = now + datetime.timedelta(days=1)
-        tomorrow = tomorrow.replace(hour=int(heure), minute = int(minute))
-        seconde = (tomorrow - now).total_seconds()
+        tomorrow = tomorrow.replace(hour=int(heure), minute=int(minute))
+        second = (tomorrow - now).total_seconds()
     elif repet == "weekly":
         nextWeek = now + datetime.timedelta(days=7)
         dayOfNextWeek = nextWeek.isoweekday()
@@ -39,30 +43,31 @@ def convertInSecond(minute, heure, jour, mois, repet):
             diff = diff * (-1)
             diff = diff + 7
             nextWeek = now + datetime.timedelta(days=diff)
-        nextWeek = nextWeek.replace(hour=int(heure), minute = int(minute))
-        seconde = (nextWeek - now).total_seconds()
+        nextWeek = nextWeek.replace(hour=int(heure), minute=int(minute))
+        second = (nextWeek - now).total_seconds()
     elif repet == "monthly":
         month_days = calendar.monthrange(now.year, now.month)[1]
         nextmonth = now + datetime.timedelta(days=month_days)
         if nextmonth.day != now.day:
             nextmonth.replace(days=1) - datetime.timedelta(days=1)
-        nextmonth = nextmonth.replace(day=int(jour),hour=int(heure), minute = int(minute))
-        seconde = (nextmonth - now).total_seconds()
+        nextmonth = nextmonth.replace(day=int(jour), hour=int(heure), minute=int(minute))
+        second = (nextmonth - now).total_seconds()
     elif repet == "yearly":
-        nextYear = now.replace(year=now.year + 1, month=int(mois),day=int(jour),hour=int(heure), minute = int(minute))
-        seconde = (nextYear - now).total_seconds()
-        print (nextYear)
+        nextYear = now.replace(year=now.year + 1, month=int(mois), day=int(jour), hour=int(heure), minute=int(minute))
+        second = (nextYear - now).total_seconds()
 
-    return seconde
+    return int(second)
 
-#Execution de la commande
-def handler(commande):
-    os.system(commande)
 
-                                                ########
-                                                # MAIN #
-                                                ########
+# Execution of the command
+def handler(signal, frame):
+    # os.system(command)
+    print('ok')
 
+
+##########
+#  MAIN  #
+##########
 
 commandList = readFile()
 
@@ -75,13 +80,12 @@ for line in commandList:
     repet = line[5]
     command = line[6]
 
-    seconde = convertInSecond(minute, heure, jour, mois, repet)
+    seconds = convertInSecond(minute, heure, jour, mois, repet)
 
-    if(boolean_alarm == "0"):
-        #On creer une alarme qui s'activera dans x secondes et executera le handler
-        signal.signal(signal.SIGALRM, handler(command))
-        signal.alarm(seconde)
-
+    if boolean_alarm == "0":
+        # Create an alarm that will activate in x seconds and execute the manager
+        signal.signal(signal.SIGALRM, handler)
+        signal.alarm(seconds)
 
 while True:
     continue
