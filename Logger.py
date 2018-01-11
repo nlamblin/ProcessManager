4149e3c5215b6b629f88e1f27ce5098ed874c30b
Maxime Dolet <maxime.dolet8@etu.univ-lorraine.fr>
2018-01-11 22:52:22 +0100

    Changed severity levels for logInfo

--- Logger.py
+++ Logger.py
@@ -7,19 +7,22 @@ STDERR_FILE = 'STDERR'
 
 
 def logMessage(message, file, is_fatal):
-    if is_fatal:
+    if is_fatal == 0:
         message = 'FATAL: ' + message
 
-    else:
+    elif is_fatal == 1:
         message = 'WARNING: ' + message
+
+    elif is_fatal == 2:
+        message = 'INFO: ' + message
 
     info_file = open(file, 'a')
     info_file.write(file)
     info_file.close()
 
 
-def logInfo(info_message, stdout_log, is_fatal):
-    logMessage(info_message, STDOUT_FILE, is_fatal)
+def logInfo(info_message, stdout_log):
+    logMessage(info_message, STDOUT_FILE, 2)
     if stdout_log:
         print(info_message)
 
