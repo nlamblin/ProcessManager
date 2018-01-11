680e7abdf675573bae5db1975f038c171665f23f
Nicolas Lamblin <nicolas.lamblin2@gmail.com>
2018-01-12 00:13:07 +0100

    Add time.slee(1) to uninstall program

--- uninstall.py
+++ uninstall.py
@@ -4,10 +4,17 @@
 import os
 
 # kill the service
+import time
+
+print('Kill ProcessManager service.....')
 os.system('systemctl kill processManager.service')
 
 # remove the service file to /etc/systemd/system/
+time.sleep(1)
+print('Remove the service file.....')
 os.system('rm /etc/systemd/system/processManager.service')
 
 # remove ProcessManager to /usr/local/bin
+time.sleep(1)
+print('Remove sources of ProcessManager.....')
 os.system('rm -r /usr/local/bin/ProcessManager')
