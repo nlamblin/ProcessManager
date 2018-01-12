01b5ba7e71546a6d075d5a48f4c9a22d10633759
Nicolas Lamblin <nicolas.lamblin2@gmail.com>
2018-01-13 00:51:34 +0100

    Use semaphore

--- gobatch.py
+++ gobatch.py
@@ -2,19 +2,25 @@
 # -*- coding: utf-8 -*
 
 import os
+import atexit
 import posix_ipc as pos
 
+from logger import logError
+from logger import logInfo
+from pgcycl import useSemaphore
+from pgcycl import listAllTasks
 from datetime import datetime
 from time import sleep
 
 
 # Function to read the fbatch file
 def readFile():
-    with open('fbatch', 'r') as f:
-        array = []
-        for line in f:
-            line = line.rstrip()
-            array.append(line.split('\t'))
+    list = useSemaphore(listAllTasks, False, False)
+    array = []
+    for line in list:
+        line = line.rstrip()
+        array.append(line.split('\t'))
+
     return array
 
 
@@ -29,28 +35,62 @@ def convertFileToString():
     return stringToSend
 
 
-# change the working directory
-# os.chdir('/usr/local/bin/ProcessManager')
+# Get semaphore FBatch-Updated
+def checkIfUpdate():
+    try:
+        # Creating semaphore
+        logInfo('[GOBATCH]: Creating semaphore', False)
+        semaphore = pos.Semaphore('/FBatch_Updated', pos.O_CREAT | pos.O_EXCL, initial_value=1)
+        logInfo('[GOBATCH]: Created semaphore', False)
+
+    except pos.ExistentialError:
+        # Semaphore already created
+        logInfo('[GOBATCH]: Semaphore already created', False)
+        semaphore = pos.Semaphore('/FBatch_Updated', pos.O_CREAT)
+        logInfo('[GOBATCH]: Using existing semaphore', False)
+
+    return semaphore
+
+
+# Handler to atexit function
+def exit_handler():
+    pos.Semaphore('/FBatch_Updated', pos.O_CREAT).unlink()
+
 
 # creating the queue
 queueContentFile = pos.MessageQueue('/queue', pos.O_CREAT)
 stringToSend = convertFileToString()
 
+# Use when gobatch.py is kill
+atexit.register(exit_handler)
 
 while True:
 
-    if #semaphore
+    # get the semaphore
+    semaphore = checkIfUpdate()
+
+    # try to get the semaphore to know if the file has been updated
+    try:
+        semaphore.acquire(0)
         stringToSend = convertFileToString()
-        # changerSemaphore
 
-    # wait until the next minute
-    sleep(60 - datetime.utcnow().second)
+        # clear the semaphore
+        while semaphore.acquire(0):
+            continue
 
-    if os.fork() == 0:
-        # send the file content
-        queueContentFile.send(stringToSend, 1)
+    except pos.BusyError:
+        logInfo('[GOBATCH]: Semaphore acquired', False)
 
-        # executs findCommand.py program
-        os.execl('findCommand.py', 'a')
+    finally:
+        # wait until the next minute
+        sleep(60 - datetime.utcnow().second)
+
+        logInfo('[GOBATCH]: Creating child process', False)
+        if os.fork() == 0:
+            # send the file content
+            queueContentFile.send(stringToSend, 1)
+
+            # executs findCommand.py program
+            os.execl('findCommand.py', 'a')
 
     continue
